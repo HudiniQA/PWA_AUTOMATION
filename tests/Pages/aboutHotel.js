42f000e97a40fb3ed24ff307d7cb930d5ed2a75c
Raviteja <uraviteja35@gmail.com>
2024-11-24 17:43:07 +0530

    minor text changes and messages added

--- tests/Pages/aboutHotel.js
+++ tests/Pages/aboutHotel.js
@@ -12,9 +12,9 @@ export class AboutHotel extends BaseClass {
     #phoneCTA
     #locationPinCTA
 
-    constructor(page) { 
+    constructor(page) {
         super();
-        this.page = page; 
+        this.page = page;
     }
 
     async initializeSelectors() {
@@ -23,9 +23,9 @@ export class AboutHotel extends BaseClass {
         this.#hotelTitle = this.page.locator('.HotelInfoDrawer_title__Djjb3');
         this.#hotelDescription = this.page.locator('.HotelInfoDrawer_body__h1M_G');
         this.#redirectionLink = this.page.getByLabel('Link')
-        this.#phoneCTA=this.page.getByLabel('Phone')
-        this.#emailCTA=this.page.getByLabel('Email')
-        this.#locationPinCTA=this.page.locator('.HotelInfoDrawer_phoneEmailCtaWrapper__M3B4h > div:nth-child(5)')
+        this.#phoneCTA = this.page.getByLabel('Phone')
+        this.#emailCTA = this.page.getByLabel('Email')
+        this.#locationPinCTA = this.page.locator('.HotelInfoDrawer_phoneEmailCtaWrapper__M3B4h > div:nth-child(5)')
 
     }
     gethamburgerMenu() {
@@ -61,12 +61,12 @@ export class AboutHotel extends BaseClass {
         await this.initializeSelectors();
         await this.gethamburgerMenu().click();
         await this.getaboutHotelIcon().click();
-        const endPoint=testData.fairmontMakkahPWA.aboutHotel.getPropertyDetailsByHotelIdEndpont;
-        const apiKey=testData.fairmontMakkahPWA.aboutHotel.getPropertyDetailsByHotelIdApiKey;
+        const endPoint = testData.fairmontMakkahPWA.aboutHotel.getPropertyDetailsByHotelIdEndpont;
+        const apiKey = testData.fairmontMakkahPWA.aboutHotel.getPropertyDetailsByHotelIdApiKey;
         const query = testData.fairmontMakkahPWA.aboutHotel.getPropertyDetailsByHotelIdQuery;
-        const hotelId=testData.fairmontMakkahPWA.hotelId
+        const hotelId = testData.fairmontMakkahPWA.hotelId
         const variables = {
-            hotelId:hotelId
+            hotelId: hotelId
         };
 
         // Make API request to capture the actual data
@@ -77,53 +77,56 @@ export class AboutHotel extends BaseClass {
             },
             headers: {
                 'Content-Type': 'application/json',
-                'x-api-key':apiKey
+                'x-api-key': apiKey
             }
         });
         const responseBody = await response.json();
+        if (responseBody) {
+            console.log(' API call was successful.✅ ');
+        } else {
+            console.error(' API call failed. Response body is null or undefined.❌');
+        }
         // console.dir(responseBody, { depth: null });
         expect(response.ok()).toBeTruthy();
         const actualHotelTitle = await this.gethotelTitle().textContent();
         const actualDescription = await this.gethotelDescription().textContent();
-       
+
         //validating the hotel title and description
-        const hotelObject=responseBody.data.getPropertyDetailsByHotelId.hotel;
+        const hotelObject = responseBody.data.getPropertyDetailsByHotelId.hotel;
         expect(actualHotelTitle).toBe(hotelObject.name);
         expect(actualDescription).toBe(hotelObject.description);
-        
+
         //Validating Call,Email and URL CTA buttons
-        const hotelInformation=responseBody.data.getPropertyDetailsByHotelId.hotel.information;//Extracting the information array from the response
-        for(const info of hotelInformation)
-        {
-            if(info.type==='EMAIl')
-            {
+        const hotelInformation = responseBody.data.getPropertyDetailsByHotelId.hotel.information;//Extracting the information array from the response
+        for (const info of hotelInformation) {
+            if (info.type === 'EMAIl') {
                 await expect(this.getemailCTA()).toBeVisible()
+                console.log(`Email CTA is visible for ${actualHotelTitle} ✅ `)
             }
-            else if(info.type==='PHONE')
-            {
+            else if (info.type === 'PHONE') {
                 await expect(this.getphoneCTA().first()).toBeVisible()
+                console.log(`Phone CTA is visible for ${actualHotelTitle} ✅ `)
             }
-            else if(info.type==='URL')
-            {
+            else if (info.type === 'URL') {
                 await expect(this.getredirectionLink()).toBeVisible();
                 const pagePromise = this.context.waitForEvent('page')
                 await this.getredirectionLink().click()
                 const newPage = await pagePromise;
                 const actualRedirectUrl = newPage.url()
                 expect(actualRedirectUrl).toBe(info.value)
+                console.log(`Redirect link verfied successfully ✅ `)
                 newPage.close()
             }
         }
 
         //Validating Location CTA button
-        const hotelLocation=responseBody.data.getPropertyDetailsByHotelId.hotel.location;
+        const hotelLocation = responseBody.data.getPropertyDetailsByHotelId.hotel.location;
         // console.log(hotelLocation)
-        if (hotelLocation.latitude && hotelLocation.longitude) 
-        {
+        if (hotelLocation.latitude && hotelLocation.longitude) {
             await expect(this.getlocationPinCTA()).toBeVisible();
-        } else 
-        {
+            console.log(`Location pin is visible for ${actualHotelTitle} ✅ `)
+        } else {
             console.warn('Latitude and/or Longitude is missing, skipping location pin verification.');
-        } 
+        }
     }
 }
